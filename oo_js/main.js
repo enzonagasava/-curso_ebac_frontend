57a1ede09c0598118cc3515b041700e098581519
enzo <enzonagasava@gmail.com>
2024-09-04 14:34:42 -0300

    adicionando outra call

--- oo_js/main.js
+++ oo_js/main.js
@@ -12,18 +12,19 @@ function CelularCaract(cor, modelo, memoria, marca){
     Celular.call(this, marca)
 }
 
-function Aplicativos(Banco, Jogos){
+
+function Aplicativos(Banco, Jogos, marca){
     this.Banco = Banco;
     this.Jogos = Jogos;
     this.appDoJoao = function(){
-        return `O Joao tem um celular da marca ${celularDoJoao2.marca}, do modelo ${celularDoJoao2.modelo}, na cor ${celularDoJoao2.cor} com ${celularDoJoao2.memoria} de memoria ram. E os apps instalados no celular dele é: ${aplicativosDoJoao.Banco} e ${aplicativosDoJoao.Jogos}`
+        return `O Joao tem um celular da marca ${this.marca}, do modelo ${celularDoJoao2.modelo}, na cor ${celularDoJoao2.cor} com ${celularDoJoao2.memoria} de memoria ram. E os apps instalados no celular dele é: ${aplicativosDoJoao.Banco} e ${aplicativosDoJoao.Jogos}`
     }
-
+    Celular.call(this, marca)
 }
 
 const celularDoJoao = new Celular('Samsung')
 const celularDoJoao2 = new CelularCaract( 'azul', 'S24', '8gb','Samsung')
-const aplicativosDoJoao = new Aplicativos('NuBank', 'Jogo')
+const aplicativosDoJoao = new Aplicativos('NuBank', 'Jogo', 'Samsung')
 console.log(celularDoJoao)
 console.log(celularDoJoao2)
 console.log(aplicativosDoJoao)

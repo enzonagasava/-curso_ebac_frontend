1457bcd9fd9fcc4ff51da60f6dd04fa32f56bb0f
enzo <enzonagasava@gmail.com>
2024-09-09 13:48:48 -0300

    exercicio es6

---
+++ es6/src/exercicio_es6.js
@@ -0,0 +1,18 @@
+const alunos = [
+    { nome: 'Stefany', nota: 8 },
+    { nome: 'Gustavo', nota: 5 },
+    { nome: 'Ricardo', nota: 6 },
+    { nome: 'Pedro', nota: 7 },
+    { nome: 'Joana', nota: 4 }
+];
+
+function alunosAprovados(alunos) {
+    return alunos.filter(aluno => aluno.nota >= 6);
+}
+
+const aprovados = alunosAprovados(alunos);
+console.log(aprovados);
+
+
+
+

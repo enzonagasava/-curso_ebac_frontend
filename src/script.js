0b97e6ba5104eecae1e1fcc49c097d95a3c1c03f
enzo <enzonagasava@gmail.com>
2024-09-16 15:25:26 -0300

    typescript

---
+++ src/script.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(num1, num2) {
+    return num1 * num2;
+}
+function concatenaçao(nome) {
+    return 'olá ' + nome;
+}
+console.log(multiplicacao(2, 2));
+console.log(concatenaçao('Isabela'));
---
+++ src/script.ts
@@ -0,0 +1,10 @@
+function multiplicacao (num1: number, num2: number){
+    return num1 * num2;
+}
+
+function concatenaçao (nome: string){
+    return 'olá ' + nome
+}
+
+console.log(multiplicacao(2,2))
+console.log(concatenaçao('Isabela'))
